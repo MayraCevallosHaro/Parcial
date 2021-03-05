b19b5abffff11f8760c1f71f1a446913e7197536
MayraCevallosHaro <57886405+MayraCevallosHaro@users.noreply.github.com>
2021-03-04 21:39:43 -0500

    .

--- app/src/main/java/com/example/parcial/MainActivity.java
+++ app/src/main/java/com/example/parcial/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.parcial;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imagen = (ImageView) findViewById(R.id.ivgallery);
+    }
+
+
+    public void selecImg(View v)
+    {
+
+        final CharSequence[] opciones ={"Tomar Foto","Buscar Imagen"};
+        final AlertDialog.Builder alertOpciones = new AlertDialog.Builder(MainActivity.this);
+        alertOpciones.setTitle("Seleccione...");
+        alertOpciones.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                switch (opciones[i].toString()){
+                    case "Tomar Foto":
+                        tomarFoto();
+                        break;
+
+                    case "Buscar Imagen":
+                        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/");
+                        startActivityForResult(intent.createChooser(intent,"Seleccione la app"),10);
+                        break;
+                }
+            }
+        });
+        alertOpciones.show();
+
     }
 }
